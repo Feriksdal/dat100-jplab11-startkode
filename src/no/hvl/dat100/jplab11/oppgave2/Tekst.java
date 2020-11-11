7cec922ff2c37ba96b97056cbfa281e4a737d528
Erik Fiksdal <erikfix@hotmail.com>
2020-11-11 13:08:47 +0100

    "Tekst" All green

--- src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
@@ -5,55 +5,52 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Tekst extends Innlegg {
 
-	// TODO: objektvariable 
-	
 	private String tekst;
 	
 	public Tekst () {
-		
+
 	}
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
 		
 		super(id, bruker, dato);
 		this.tekst = tekst;
-		
-		//throw new UnsupportedOperationException(TODO.constructor("Tekst"));
 	}
 	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
 		
 		super(id, bruker, dato, likes);
 		this.tekst = tekst;
-		
-		//throw new UnsupportedOperationException(TODO.constructor("Tekst"));
 	}
 	
 	public String getTekst() {
-		
-		return tekst;
-		
-		//throw new UnsupportedOperationException(TODO.method());
 
+		return tekst;
 	}
 
 	public void setTekst(String tekst) {
 		
 		this.tekst = tekst;
 		
-		//throw new UnsupportedOperationException(TODO.method());
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
-
+//		TEKST
+//		1
+//		Ole Olsen
+//		23-10-2019
+//		0
+//		en tekst
+		return ("TEKST\n" + super.getId() + "\n" + super.getBruker() + "\n" + super.getDato() + "\n" + super.getLikes() + "\n" + tekst + "\n");
+	
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
 	public String toHTML() {
 			
 		throw new UnsupportedOperationException(TODO.method());
-				
+		
 	}
+
 }
