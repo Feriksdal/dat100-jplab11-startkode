1d0be69cbc3e6f940f235ef1376a0a5e4b4b3645
Birger <Birger@BIRGER>
2020-11-11 15:49:08 +0100

    SkrivBlogg GREENLIGHTED

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,6 +1,7 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
 import java.io.FileNotFoundException;
+
 import java.io.PrintWriter;
 
 import no.hvl.dat100.jplab11.common.TODO;
@@ -9,7 +10,35 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		
+		boolean printed;
+		PrintWriter printing = null;
+		
+		try {		
+			printing = new PrintWriter(filnavn);
+			
+			for (int i = 0; i < samling.getAntall(); i++) {
+				printing.println(samling.getSamling()[i].toString());
+			
+				}
+			
+			printed = true;			
+			}
+		
+			catch(Exception e) {
 
-		throw new UnsupportedOperationException(TODO.method());
+				printed = false;
+					}
+		
+			finally {
+				
+				if (printing != null)
+				
+				printing.close();
+			}
+		
+		return printed;
+
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 }
